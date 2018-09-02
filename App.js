6b28c81a5c0a0e328b18071f0ad802715c88b558
Yamit Villamil <yamit.villamil@globant.com>
2018-09-02 12:26:48 -0500

    Intro To Chatkit

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import React from 'react'
-import MessageList from './components/MessageList'
-import SendMessageForm from './components/SendMessageForm'
-import RoomList from './components/RoomList'
-import NewRoomForm from './components/NewRoomForm'
+import React from 'react';
+import MessageList from './components/MessageList';
+import SendMessageForm from './components/SendMessageForm';
+import RoomList from './components/RoomList';
+import NewRoomForm from './components/NewRoomForm';
+import { ChatManager, TokenProvider } from '@pusher/chatkit';
+import { tokenUrl, instanceLocator } from './config';
+
+const tokenProvider = new TokenProvider({
+  url: tokenUrl,
+});
+
+const chatManager = new ChatManager({
+  instanceLocator,
+  userId: 'yamitrvg12',
+  tokenProvider,
+});
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="app">
-                <RoomList />
-                <MessageList />
-                <SendMessageForm />
-                <NewRoomForm />
-            </div>
-        );
-    }
+  componentDidMount() {
+    chatManager
+      .connect()
+      .then((currentUser) => {
+        // The currentUser is our interface for talking with the Chat API.
+        currentUser.subscribeToRoom({
+          roomId: 15276341,
+          hooks: {
+            onNewMessage: (message) => {
+              console.log(message.text);
+            },
+          },
+        });
+      })
+      .catch((error) => {
+        console.log('Error ', error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <RoomList />
+        <MessageList />
+        <SendMessageForm />
+        <NewRoomForm />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/af799227-05d3-4a5b-ae27-8e378f5683b1/token";
-const instanceLocator = "v1:us1:af799227-05d3-4a5b-ae27-8e378f5683b1";
+const tokenUrl = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/fa2f9b29-4a8b-4401-9c64-92d01d6a6a75/token';
+const instanceLocator = 'v1:us1:fa2f9b29-4a8b-4401-9c64-92d01d6a6a75';
 
 exports.tokenUrl = tokenUrl;
 exports.instanceLocator = instanceLocator;
